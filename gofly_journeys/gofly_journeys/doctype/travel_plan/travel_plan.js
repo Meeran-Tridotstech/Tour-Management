cc34296f7977d069a01dd882183f80b05c5c34f4
sharmila jayaprakash <sharmilajayaprakash2001@gmail.com>
2025-10-28 14:06:12 +0530

    file added

--- gofly_journeys/gofly_journeys/doctype/travel_plan/travel_plan.js
+++ gofly_journeys/gofly_journeys/doctype/travel_plan/travel_plan.js
@@ -4,6 +4,10 @@ frappe.ui.form.on('Travel Plan', {
             // Just copy the value directly
             frm.set_value('pickup', frm.doc.to_airport);
             frm.set_value('travel_date', frm.doc.arrival_date);
+            frm.set_value('departure_date', frm.doc.travel_start_date);
+            frm.set_value('return_departure_date', frm.doc.travel_end_date);
+
+
         } else {
             frm.set_value('pickup', '');
             frm.set_value('travel_date', '');
